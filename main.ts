f3cb75da6d23764bb52224582ef93b973b515583
DanielMalede <danielmalede@gmail.com>
2022-07-24 13:32:10 +0300

    .

---
+++ main.ts
@@ -0,0 +1,44 @@
+// interface Ivehicle {
+//     modle:string
+//     num_door:number;
+//     num_wheels:number;
+// }
+// const newvehicle:Ivehicle = {
+//     modle:"Jaguaar",
+//     num_door:4,
+//     num_wheels:4
+// }
+// const newvehicle1:Ivehicle = {
+//     modle:"BMW",
+//     num_door:6,
+//     num_wheels:8
+// }
+// class CarStore {
+//     city:string;
+//     state:string;
+//     storeName:string;
+//     vehicle:Ivehicle[];
+//     constructor(city:string,state:string,storeName:string,vehicle:Ivehicle[]){
+//         this.city=city
+//         this.state=state
+//         this.storeName=storeName
+//         this.vehicle=vehicle
+//     }
+// }
+// const newJaguar = new CarStore("jerusalem","israel","JaguarWW",[newvehicle,newvehicle1])
+// console.log(newJaguar)
+
+// class Grage {
+//     garge_Name:string;
+//     owner_name:string;
+//     yatzaTzadik:boolean;
+//     vehicle:Ivehicle[];
+//     constructor(garge_Name:string,owner_name:string,yatzaTzadik:boolean,vehicle:Ivehicle[]){
+//         this.garge_Name=garge_Name
+//         this.owner_name=owner_name
+//         this.yatzaTzadik=yatzaTzadik
+//         this.vehicle=vehicle
+//     }
+// }
+// const newGarge = new Grage ("FixHere","daniel",true,[newvehicle,newvehicle1])
+// console.log(newGarge)
